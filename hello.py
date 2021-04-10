9ac63e575b0ac00cb06052910c71956ed2febda9
Lee-Min-Jung <light2009@sookmyung.ac.kr>
2021-04-10 18:43:57 +0900

    plus explanation

--- hello.py
+++ hello.py
@@ -1,16 +1,20 @@
+#Quantity of coffee and juice at first
 initialCoffee=3
 initialJuice=2
 while True:
+#When there is no coffee and juice
     if initialCoffee==0 and initialJuice==0:
         print("All sold out. Thank you for using \"Hello Cafe\".")
         break
+#When there are some coffee or juice left
     elif initialCoffee>0 or initialJuice>0:
-        print("**************")
+        print("*"*20)
         print("Hello Cafe")
-        print("**************")
+        print("*"*20)
         print("1. Coffee")
         print("2. Juice")
-        menuSelect=input("Please make a selection:") 
+        menuSelect=input("Please make a selection:")
+#When customer choose coffee(menu1) 
         if menuSelect=="1":
             if initialCoffee==0:
                 print("There is no left coffee.")
@@ -25,6 +29,7 @@ while True:
                     elif coffeeQuantity>initialCoffee:
                         print("There is only {} left.".format(initialCoffee))
                         continue
+#When customer choose juice(menu2)
         if menuSelect=="2":
             if initialJuice==0:
                 print("There is no left juice.")
