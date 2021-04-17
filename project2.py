98f1302ad868e8683765774bfce764c555d7ccbf
Lee-Min-Jung <light2009@sookmyung.ac.kr>
2021-04-17 18:06:58 +0900

    first result

--- project2.py
+++ project2.py
@@ -1,2 +1,29 @@
-print("hello")
-print("h")
+
+def main():
+    mid=float(input("Enter grade on midterm:"))
+    final=float(input("Enter grade on final exam:"))
+    print("Semester Grade: {}".format(semesterGrade(mid,final)))
+
+def semesterGrade(mid,final):
+    ave=(mid+final*2)/3
+    ave2=ceil(ave)
+    if ave2>=90:
+        return "A"
+    elif ave2>=80:
+        return "B"
+    elif ave2>=70:
+        return "C"
+    elif ave2>=60:
+        return "D"
+    else:
+        return "F"
+
+def ceil(ave):
+    ave2=round(ave)
+    return ave2
+
+main()
+
+
+
+
